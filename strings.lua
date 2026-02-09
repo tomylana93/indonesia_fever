3fb8bad960b1bb8004701041501c7052b5e5f1cf
Tomy <stealthflanker93@gmail.com>
2026-02-09 19:04:45 +0700

    remove Indonesian localization strings from strings.lua

--- strings.lua
+++ strings.lua
@@ -4,9 +4,5 @@ return {
 		["mod_name"] = "Indonesia Fever",
 		["mod_desc"] = "A mod focused on Indonesian-themed content for Transport Fever 2.",
 	},
-	id = {
-		["mod_name"] = "Indonesia Fever",
-		["mod_desc"] = "Mod yang berfokus pada konten bertema Indonesia untuk Transport Fever 2.",
-	},
 }
 end
